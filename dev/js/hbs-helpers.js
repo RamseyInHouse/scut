67ccbd2f369649a6d4f2d720e79b7bd8faa7448b
David Clark <david.dave.clark@gmail.com>
2013-09-26 22:07:05 -0700

    Split up items in documentation.

--- dev/js/hbs-helpers.js
+++ dev/js/hbs-helpers.js
@@ -3,8 +3,23 @@ var grunt = require('grunt');
 module.exports.register = function(Handlebars, options) {
 
   Handlebars.registerHelper('slugToWords', function(str) {
-    var noDash = str.replace(/-/g, ' ');
-    return noDash;
+    var noHyphens = str.replace(/_/g, ' '),
+        words = noHyphens.split(' '),
+        specialWords = [
+          'list',
+          'positioning',
+          'v-center',
+          'link'
+        ];
+
+    if (grunt.util._.contains(specialWords, words[0])) {
+      var start = words.shift() + ':';
+      words.unshift(start);
+      return words.join(' ');
+    } else {
+      return noHyphens;
+    }
+
   });
 
   Handlebars.registerHelper('getExampleScss', function(name) {
